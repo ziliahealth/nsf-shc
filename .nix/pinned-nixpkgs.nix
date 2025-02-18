034c6649c54a3f69dfe976d26faa734247da7ec3
Gabriel Boucher <gabriel.boucher@ziliahealth.com>
2025-02-18 13:03:33 -0500

    Move comment above the variable

--- .nix/pinned-nixpkgs.nix
+++ .nix/pinned-nixpkgs.nix
@@ -1,7 +1,8 @@
 {}:
 let
   nixpkgs = rec {
-    rev = "030ba1976b7c0e1a67d9716b17308ccdab5b381e"; # 24.11 - 2025-02-04
+    # nixpkgs 24.11 - 2025-02-04
+    rev = "030ba1976b7c0e1a67d9716b17308ccdab5b381e";
     sha256 = "14rpk53mia7j0hr4yaf5m3b2d4lzjx8qi2rszxjhqq00pxzzr64w";
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     src = builtins.fetchTarball {
